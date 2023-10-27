1fc83d7b34b2643421ce8e52c6b78fa11b7a4f4c
Aayush <88710500+kaayush163@users.noreply.github.com>
2023-10-27 12:26:31 +0530

    upload files multer use

--- routes/message.js
+++ routes/message.js
@@ -5,7 +5,7 @@ const multer=require('multer')
 const userAuthenticate=require('../middleware/auth')
 const Userchat=require('../controllers/messageController');
 const upload=multer();
-
+//for uploading files use multer 
 
 routes.post('/sendmessage',userAuthenticate.authenticate,Userchat.addMessage);
 routes.get('/:groupId',userAuthenticate.authenticate,Userchat.getMessages);
